d8fb31def6466c9b339d1a86580d7d3b9e4da463
Kevin Keylock <kevinkeylock@hotmail.com>
2024-02-09 17:20:46 -0800

    Create add, minus, multiply, divide functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add (x, y){
+  return x + y;
+}
+
+function minus (x, y){
+  return x - y;
+}
+
+function multiply (x, y){
+  return x * y;
+}
+
+function divide (x, y){
+  return x / y;
+}
+
