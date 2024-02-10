31f44a9ba212ff4004e7c898087b4e5c19195bb8
Kevin Keylock <kevinkeylock@hotmail.com>
2024-02-10 11:23:39 -0800

    Create number and operator variables.

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+let firstNumber = 1;
+let secondNumber = 1;
+let operator = '+';
+
+function operate(firstNumber, operator, secondNumber){
+  if(operator === '+'){
+    add(firstNumber, secondNumber);
+  }else if(operator === '-'){
+    subtract(firstNumber, secondNumber);
+  }else if(operator === '*'){
+    multiply(firstNumber, secondNumber);
+  }else if(operator === '/'){
+    divide(firstNumber, secondNumber);
+  }
+  
+}
+
 function add (x, y){
-  return x + y;
+  let answer = x + y;
+  console.log(answer)
 }
 
 function subtract (x, y){
-  return x - y;
+  let answer = x - y;
+  console.log(answer)
 }
 
 function multiply (x, y){
-  return x * y;
+  let answer = x * y;
+  console.log(answer)
 }
 
 function divide (x, y){
-  return x / y;
+  let answer = x / y;
+  console.log(answer)
 }
 
+operate(1, '*', 2)
