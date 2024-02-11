5970d698ded72b12ae526dab0ba84129aa4490a9
Kevin Keylock <kevinkeylock@hotmail.com>
2024-02-10 16:49:13 -0800

    Make number clicks store number in an object

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ const nine = document.querySelector('#nine');
 const clear = document.querySelector('#clear')
 const display = document.getElementById('display');
 const plus = document.querySelector('#plus')
-
-const firstNumber = 0;
-const secondNumber = 0;
+const numbers = {
+  firstNumber: 0,
+  secondNumber: 0
+}
 const operator = '';
 
 clear.addEventListener("click", function(){
@@ -25,73 +26,81 @@ one.addEventListener("click", function(){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  } numbers.firstNumber = Number(display.textContent);
+})
 two.addEventListener("click", function(){
   const displayTotal = 2;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  } numbers.firstNumber = Number(display.textContent);
+})
 three.addEventListener("click", function(){
   const displayTotal = 3;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  } numbers.firstNumber = Number(display.textContent);
+})
 four.addEventListener("click", function(){
   const displayTotal = 4;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  } numbers.firstNumber = Number(display.textContent);
+})
 five.addEventListener("click", function(){
   const displayTotal = 5;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  }numbers.firstNumber = Number(display.textContent);
+})
 six.addEventListener("click", function(){
   const displayTotal = 6;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+  } numbers.firstNumber = Number(display.textContent);
+})
 seven.addEventListener("click", function(){
   const displayTotal = 7;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-}})
+} numbers.firstNumber = Number(display.textContent);})
 eight.addEventListener("click", function(){
   const displayTotal = 8;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  }})
+  } numbers.firstNumber = Number(display.textContent);
+})
 nine.addEventListener("click", function(){
   const displayTotal = 9;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  }})
+  } numbers.firstNumber = Number(display.textContent);
+})
 zero.addEventListener("click", function(){
   const displayTotal = 0;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  }})
+  } numbers.firstNumber = Number(display.textContent);
+})
 
-const displayNumber = Number(display.textContent)
-
+console.log(numbers)
 /*plus.addEventListener("click", function(){
   const firstNumber = display.textContent;
   console.log (firstNumber);
