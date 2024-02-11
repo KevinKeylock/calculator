898242903a47fa46b0b8db7f3a458c9f18caac89
Kevin Keylock <kevinkeylock@hotmail.com>
2024-02-10 17:36:17 -0800

    Make operator buttons work

--- script.js
+++ script.js
@@ -11,11 +11,18 @@ const nine = document.querySelector('#nine');
 const clear = document.querySelector('#clear')
 const display = document.getElementById('display');
 const plus = document.querySelector('#plus')
+const equals = document.querySelector('#equals')
+const minus = document.querySelector('#minus')
+const times = document.querySelector('#times')
+const divide = document.querySelector('#divide')
+
 const numbers = {
   firstNumber: 0,
   secondNumber: 0
 }
-const operator = '';
+const operator = {
+  operator: '', 
+};
 
 clear.addEventListener("click", function(){
   display.innerHTML = 0;
@@ -26,109 +33,121 @@ one.addEventListener("click", function(){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 two.addEventListener("click", function(){
   const displayTotal = 2;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 three.addEventListener("click", function(){
   const displayTotal = 3;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 four.addEventListener("click", function(){
   const displayTotal = 4;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 five.addEventListener("click", function(){
   const displayTotal = 5;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  }numbers.firstNumber = Number(display.textContent);
-})
+}})
 six.addEventListener("click", function(){
   const displayTotal = 6;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 seven.addEventListener("click", function(){
   const displayTotal = 7;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-} numbers.firstNumber = Number(display.textContent);})
+}})
 eight.addEventListener("click", function(){
   const displayTotal = 8;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 nine.addEventListener("click", function(){
   const displayTotal = 9;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
-})
+}})
 zero.addEventListener("click", function(){
   const displayTotal = 0;
   if (display.textContent === '0'){
     display.innerHTML = displayTotal;
   } else {
     display.textContent = `${display.textContent}${displayTotal}`;
-  } numbers.firstNumber = Number(display.textContent);
+}})
+
+plus.addEventListener("click", function(){
+  numbers.firstNumber = Number(display.textContent);
+  display.textContent = 0;
+  operator.operator = 'plus';
+})
+minus.addEventListener("click", function(){
+  numbers.firstNumber = Number(display.textContent);
+  display.textContent = 0;
+  operator.operator = 'minus';
+})
+times.addEventListener("click", function(){
+  numbers.firstNumber = Number(display.textContent);
+  display.textContent = 0;
+  operator.operator = 'times';
+})
+divide.addEventListener("click", function(){
+  numbers.firstNumber = Number(display.textContent);
+  display.textContent = 0;
+  operator.operator = 'divide';
 })
 
-console.log(numbers)
-/*plus.addEventListener("click", function(){
-  const firstNumber = display.textContent;
-  console.log (firstNumber);
-  return firstNumber
-  
-})*/
+equals.addEventListener("click", function(){
+  numbers.secondNumber = Number(display.textContent)
+  numberOne = numbers.firstNumber;
+  numberTwo = numbers.secondNumber;
+  operatorOne = operator.operator
+  operate(numberOne, operatorOne, numberTwo)
+})
 
 function operate(firstNumber, operator, secondNumber){
-  if(operator === '+'){
-    add(firstNumber, secondNumber);
-  }else if(operator === '-'){
+  if(operator === 'plus'){
+    add (firstNumber, secondNumber);
+  }else if(operator === 'minus'){
     subtract(firstNumber, secondNumber);
-  }else if(operator === '*'){
+  }else if(operator === 'times'){
     multiply(firstNumber, secondNumber);
-  }else if(operator === '/'){
-    divide(firstNumber, secondNumber);
+  }else if(operator === 'divide'){
+    division(firstNumber, secondNumber);
   }}
 
 function add (x, y){
   let answer = x + y;
-  console.log(answer)
+  display.textContent = `${answer}`;
 } function subtract (x, y){
   let answer = x - y;
-  console.log(answer)
+  display.textContent = `${answer}`;
 } function multiply (x, y){
   let answer = x * y;
-  console.log(answer)
-} function divide (x, y){
+  display.textContent = `${answer}`;
+} function division (x, y){
   let answer = x / y;
-  console.log(answer)
+  display.textContent = `${answer}`;
 }
